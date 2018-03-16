707fd9d1ad2453f6089ea071de4f63b4dfd3fc3a
legendddhgf <legendddhgf@gmail.com>
2018-03-16 03:58:41 -0700

    automatically analyze all results dirs

--- project/implementation/analyze_all.sh
+++ project/implementation/analyze_all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-if [ ! -e results ]; then
+
+num_results=$(ls | grep '_results$')
+if [ "${num_results[0]}" == "" ]; then
   echo "please run RocksBench to generate results first"
   exit 1
 fi
 
-results=$(ls results)
-for test_type in $results; do
-  tests=$(ls results/$test_type)
-  for test in $tests; do
-    ext=$(echo $test | cut -d '.' -f 2)
-    if [ ! "$ext" = "csv" ]; then
-      continue
-    fi
-    ./analyze_results.sh results/$test_type/$test
+
+for results in $(ls | grep '_results$'); do
+  for test_type in $(ls $results); do
+    tests=$(ls $results/$test_type)
+    for test in $tests; do
+      ext=$(echo $test | cut -d '.' -f 2)
+      if [ ! "$ext" = "csv" ]; then
+        continue
+      fi
+      ./analyze_results.sh $results/$test_type/$test
+    done
   done
 done
 
